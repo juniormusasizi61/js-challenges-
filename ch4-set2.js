3137d38c262de419700dccef3959aa91389a76b5
juniormusasizi61 <juniormusasizi6@gmail.com>
2024-08-14 09:49:05 +0300

    debug

--- ch4-set2.js
+++ ch4-set2.js
@@ -6,12 +6,12 @@ function singleNumber(nums) {
         single ^= num; // XOR the current number with the result
     }
 
-    return single; // The result will be the single number
+    return single;
 }
 
 // Example usage
 const nums1 = [2, 2, 1];
 const nums2 = [4, 1, 2, 1, 2];
 
-console.log(singleNumber(nums1)); // Output: 1
-console.log(singleNumber(nums2)); // Output: 4
+console.log(singleNumber(nums1)); 
+console.log(singleNumber(nums2)); 
